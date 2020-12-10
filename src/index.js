31196f2f3b19931427a4da3de0064d9e600cf47e
alexperez-cst <palex4490@gmail.com>
2020-12-10 17:09:37 +0100

    File Organization Ended!

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*
+	# The 'todos' are going to be objects created with classes
+	# A 'todo' will have:
+		*Title
+		*Description
+		*dueDate (fecha de finalizacion)
+		*priority
+	# Can create more than a todo list and a default list with all the 'todos'
+	# Separar el programa en diferentes modulos:
+		*Creacion de todos
+		*Dom manipulation
+		*seleccionar todos como finalizado
+		*crear nuevas listas de todos
+	# Guardar los datos en el local storage
+	# usar date-fns para las fechas de los todos
+*/
